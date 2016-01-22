72b343893004a02e927c24265bc2074b9384206d
JasonMDev <jasonmansdev@gmail.com>
2016-01-22 13:10:23 +0000

    Added new orders controller manually including routes and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :products
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   # get 'static_pages/index'
 
   get 'static_pages/about'
 
   get 'static_pages/contact'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
