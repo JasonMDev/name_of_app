b25c653f1cbdb0adc70f92b6e1a3ee767f95ea42
JasonMDev <jasonmansdev@gmail.com>
2016-01-22 15:07:34 +0000

    Added note.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+	# You can also specify the layout at the top of your controller 
+	# and it will apply that layout to all of the actions within that controller.
+	# layout "static"
+
 
 	def landing_page
 		@featured_product = Product.first
