95221f468a1c3c2671d0ba36d0018c9d4a3dbd09
JasonMDev <jasonmansdev@gmail.com>
2016-01-22 08:19:10 +0000

    Added seed file to the folders.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |n|
+
+    Product.create(
+        :name => "Seed Generated Product #{n}", 
+        :description => "I created this bike through the seed file.", 
+        :image_url => "img/product#{n}.jpg",
+        :color => "blue")
+end
+
