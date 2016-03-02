29c146713adb0be19b75c06d17be7f8d6513be0e
JasonMDev <jasonmansdev@gmail.com>
2016-03-02 20:03:44 +0000

    Added the admin flag and restricted users to just readin and creating comments .

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,12 @@ class Ability
   def initialize(user)
 
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id 
+    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, Comment
+    else
+      can [:read, :create ], Comment
+    end 
 
 
     # Define abilities for the passed in user here. For example:
