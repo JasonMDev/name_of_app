43217ec1f2fce311f4f6b27324ba993261b5af4d
JasonMDev <jasonmansdev@gmail.com>
2016-03-20 15:09:01 +0000

    Add stary items saved along the way.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,12 @@ class Product < ActiveRecord::Base
 	has_many :comments
   validates :name, presence: true
 
+  # Raty average rating
   def average_rating
     comments.average(:rating).to_f
   end
-
+  
+  # Raty total rating.
   def total_rating
     comments.count(:rating).to_f
   end
