a15f4d664c645b6490c65d2ba03ef1b42c63ed47
JasonMDev <jasonmansdev@gmail.com>
2016-09-16 15:32:15 +0100

    Add the app/models/application_record.rb as per 2.2 of Rails upgrade guide.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#landing_page'
+  get 'static_pages/index'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  post 'static_pages/thank_you'
+  post 'payments/create'
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations"}
+  resources :users
+  
+  resources :products do
+    resources :comments
+  end  
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
