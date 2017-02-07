0c9d9d6c6089f9c0e95dc45a7eec38c10ff7cb0d
JasonMDev <jasonmansdev@gmail.com>
2017-02-07 21:44:37 +0000

    Checked and confirm mailer is functioning.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -5,7 +5,14 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at 
   # http://localhost:3000/rails/mailers/user_mailer/contact_form
   def contact_form
-  	UserMailer.contact_form("Jack", "js@example.com", "Hello world!" )
+    UserMailer.contact_form("Jack", "js@example.com", "Hello world!" )
+  end
+
+   # Preview this email at 
+  # http://localhost:3000/rails/mailers/user_mailer/welcome
+  def welcome
+    usertest = User.create(:email => "js@example.com")
+    UserMailer.welcome(usertest)
   end
 
 end
